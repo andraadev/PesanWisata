f4628be2667e3855ba262473bc4b219161833299
andraadev <107798601+andraadev@users.noreply.github.com>
2026-09-07 22:39:18 +0700

    refactor(home): update content and improve formatting

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,28 @@
-import React from "react";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
-import { Link } from "react-router-dom";
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <body className="no-scrollbar">
-    <Navbar/>
-    <section className="jumbotron container">
+  return (
+    <body className="no-scrollbar">
+      <Navbar />
+      <section className="jumbotron container">
         <h1 className="jumbotron-header text-shadow">PesanWisata</h1>
-        <p className="jumbotron-content text-shadow">Aplikasi ini akan membantu kamu untuk memesan destinasi wisata tertentu.</p>
-        <Link to="/destinasi" className="cta-btn btn btn-lg bg-white text-dark rounded-pill" role="button">Pesan
-            Sekarang</Link>
-    </section>
-    <Footer/>
-</body>
-    );
+        <p className="jumbotron-content text-shadow">
+          Temukan dan pesan tiket ke berbagai destinasi wisata favorit dengan cepat, mudah, dan
+          praktis dalam satu tempat.
+        </p>
+        <Link
+          to="/destinasi"
+          className="cta-btn btn btn-lg bg-white text-dark rounded-pill"
+          role="button"
+        >
+          Lihat Destinasi
+        </Link>
+      </section>
+      <Footer />
+    </body>
+  );
 };
 
 export default Home;
-
