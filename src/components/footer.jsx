33dcc5eeece51e0d44353185c8a3d805f053d59d
andraadev <107798601+andraadev@users.noreply.github.com>
2026-09-07 22:35:53 +0700

    refactor(footer): update footer structure and styles

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,9 @@
-import React from "react"
-
 const Footer = () => {
     return (
-<footer className="text-center">
-    <p>© 2024, PesanWisata App. All rights reserved </p>
-</footer>
-);
+    <footer className="p-2 fixed-bottom d-flex justify-content-center align-items-center bg-body-tertiary">
+        <p className="m-0">© 2024, PesanWisata App. All rights reserved </p>
+    </footer>
+    );
 };
 
 export default Footer;
