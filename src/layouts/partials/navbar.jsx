d54974033b77f4f3cfd7f223f3c68ee7ef5b9843
andraadev <107798601+andraadev@users.noreply.github.com>
2026-09-11 16:03:12 +0700

    feat(navbar): update links for role-based access and admin routes

--- src/layouts/partials/navbar.jsx
+++ src/layouts/partials/navbar.jsx
@@ -46,35 +46,40 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
-            <li className="nav-item">
-              <NavLink to="/" className="nav-link">
-                Beranda
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/destinasi" className="nav-link" aria-current="page">
-                Destinasi Wisata
-              </NavLink>
-            </li>
-            {token && role === 'Admin' && (
+            {role !== 'Admin' && (
               <>
                 <li className="nav-item">
-                  <NavLink className="nav-link active text-white" aria-current="page" to="/beranda">
+                  <NavLink to="/" className="nav-link">
                     Beranda
                   </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link text-white" to="/data-user">
+                  <NavLink to="/destinasi" className="nav-link">
+                    Destinasi Wisata
+                  </NavLink>
+                </li>
+              </>
+            )}
+            {token && role === 'Admin' && (
+              <>
+                <li className="nav-item">
+                  <NavLink className="nav-link active" aria-current="page" to="/admin/beranda">
+                    Beranda
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/admin/data-user">
                     Data User
                   </NavLink>
                 </li>
                 <li className="nav-item">
-                  <NavLink className="nav-link text-white" to="/data-destinasi">
+                  <NavLink className="nav-link" to="/admin/data-destinasi">
                     Data Destinasi
                   </NavLink>
                 </li>
               </>
             )}
+
             {token && role === 'User' && (
               <li className="nav-item">
                 <NavLink to="/data-booking" className="nav-link">
