3849ff828a168b8d7fb5474e3461e1c43ed0e249
andraadev <107798601+andraadev@users.noreply.github.com>
2026-09-13 22:28:29 +0700

    refactor(footer): remove fixed-bottom positioning

--- src/layouts/partials/footer.jsx
+++ src/layouts/partials/footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
-    return (
-    <footer className="p-2 fixed-bottom d-flex justify-content-center align-items-center bg-body-tertiary">
-        <p className="m-0">© 2024, PesanWisata App. All rights reserved </p>
+  return (
+    <footer className="p-2 d-flex justify-content-center align-items-center bg-body-tertiary">
+      <p className="m-0">© 2024, PesanWisata App. All rights reserved </p>
     </footer>
-    );
+  );
 };
 
 export default Footer;
