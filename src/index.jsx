a18f28e460a37f2d5a82a23b84c169bbbd4ffe6d
andraadev <107798601+andraadev@users.noreply.github.com>
2026-09-25 14:47:27 +0700

    feat(query): setup React Query

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,15 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './assets/css/custom.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
